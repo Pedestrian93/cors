e1d5276074e6eff598cc1de38ffe4f5bc41f16ae
Alex San <sanpc@qq.com>
2020-08-12 20:55:11 +0800

    feat: clean

--- public/index.js
+++ public/index.js
@@ -1,21 +1,48 @@
-const b = document.getElementById("query");
+const simpleGet = document.getElementById("get");
+const simplePost = document.getElementById("post");
+const preflightPost = document.getElementById("prePost");
 
-b.addEventListener("click", function () {
-  console.log("发送请求。。。");
+simpleGet.addEventListener("click", function () {
   axios
-    .get("/user.json")
+    .get("http://localhost:3001/user.json")
     .then(function (response) {
       console.log(response.data);
     })
     .catch(function (error) {
       console.log(error);
     });
+});
 
+simplePost.addEventListener("click", function () {
   axios
-    // .post("http://localhost:3001/user.json")
-    .post("http://localhost:3001/p", {
-      // headers: { SELF_HEADER: "xxx" },
+    .post(
+      "http://localhost:3001/p",
+      { name: "san" },
+      {
+        headers: {
+          "Content-Type": "  text/plain",
+        },
+      }
+    )
+    .then(function (response) {
+      console.log(response.data);
     })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+preflightPost.addEventListener("click", function () {
+  axios
+    .post(
+      "http://localhost:3001/p",
+      {
+        name: "san",
+      },
+      {
+        headers: { SELF_HEADER: "xxx", EE: "jj" },
+      }
+    )
     .then(function (response) {
       console.log(response.data);
     })
--- server/index.js
+++ server/index.js
@@ -9,34 +9,31 @@ app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
 
-// app2.use(
-//   express.static("public", {
-//     setHeaders: function (res, path, stat) {
-//       res.set({
-//         "x-timestamp": Date.now(),
-//         "Access-Control-Allow-Origin": "http://localhost:3000",
-//         "Access-Control-Allow-Methods": "POST,PUT",
-//       });
-//     },
-//   })
-// );
+// 简单跨域处理
+app2.use(
+  express.static("public", {
+    setHeaders: function (res, path, stat) {
+      res.set({
+        "Access-Control-Allow-Origin": "http://localhost:3000",
+      });
+    },
+  })
+);
 
-app2.post("/p", (req, res) => {
-  console.log(req, res);
-  res.set({
-    // "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Origin": "http://localhost:3000",
-    // "Access-Control-Request-Method": "POST",
-    // "Access-Control-Allow-Headers": "SELF_HEADER,Content-Type",
-  });
-  res.send("......post result");
-});
+// 携带自定义 headers 预检请求处理
 app2.options("/p", (req, res) => {
-  console.log(req, res);
   res.set({
     "Access-Control-Allow-Origin": "http://localhost:3000",
     "Access-Control-Request-Method": "POST",
-    "Access-Control-Allow-Headers": "SELF_HEADER,Content-Type",
+    "Access-Control-Allow-Headers": "SELF_HEADER,Content-Type, ee",
+    "Access-Control-Max-Age": 10,
+  });
+  res.send();
+});
+
+app2.post("/p", (req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin": "http://localhost:3000",
   });
   res.send("......post result");
 });
